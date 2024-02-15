dbb3cec14f5790a2664b0be74b5b34e2c596fba0
Mary Hope Tabunlupa <maryhope.tabunlupa@wvsu.edu.ph>
2024-02-15 08:22:35 +0800

    Update drrmo.sql

--- sql/drrmo.sql
+++ sql/drrmo.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Feb 12, 2024 at 01:08 AM
+-- Generation Time: Feb 15, 2024 at 01:22 AM
 -- Server version: 10.4.28-MariaDB
 -- PHP Version: 8.2.4
 
@@ -41,7 +41,7 @@ CREATE TABLE `usar` (
   `position` varchar(200) NOT NULL,
   `address` varchar(255) NOT NULL,
   `contact_no` int(11) NOT NULL,
-  `incident` text NOT NULL,
+  `incident` varchar(255) NOT NULL,
   `weather` varchar(50) NOT NULL,
   `cpr` varchar(50) NOT NULL,
   `defib` varchar(50) NOT NULL,
@@ -53,10 +53,24 @@ CREATE TABLE `usar` (
   `latitude` decimal(50,0) NOT NULL,
   `longitude` decimal(50,0) NOT NULL,
   `dist_ratio` decimal(50,0) NOT NULL,
-  `recommendation` text NOT NULL,
-  `narrative` text NOT NULL,
+  `recommendation` varchar(255) NOT NULL,
+  `narrative` varchar(255) NOT NULL,
   `images` varchar(255) NOT NULL,
-  `date` timestamp NOT NULL DEFAULT current_timestamp()
+  `date` timestamp NOT NULL DEFAULT current_timestamp(),
+  `no_cas` int(11) NOT NULL,
+  `amb_spec` varchar(255) NOT NULL,
+  `time_start` time NOT NULL,
+  `time_end` time NOT NULL,
+  `cycle` varchar(255) NOT NULL,
+  `cr` int(11) NOT NULL,
+  `enr` int(11) NOT NULL,
+  `atscn` int(11) NOT NULL,
+  `descn` int(11) NOT NULL,
+  `insvc` int(11) NOT NULL,
+  `optm` int(11) NOT NULL,
+  `end` int(11) NOT NULL,
+  `begin` int(11) NOT NULL DEFAULT 0,
+  `total` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
@@ -71,7 +85,7 @@ CREATE TABLE `users` (
   `last_name` varchar(50) NOT NULL,
   `email` varchar(255) NOT NULL,
   `password` varchar(255) NOT NULL,
-  `registration_date` int(11) NOT NULL
+  `registration_date` date NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
@@ -79,7 +93,10 @@ CREATE TABLE `users` (
 --
 
 INSERT INTO `users` (`id`, `first_name`, `last_name`, `email`, `password`, `registration_date`) VALUES
-(1, 'Mary', 'Tabunlupa', 'tabunlupamaryhope@gmail.com', '$2y$10$jzWs1umaVgKFIWb3Z7jgzuzNxa1KmNvTYwttVrNgRxaswXnL/bhyC', 0);
+(1, 'Mary', 'Tabunlupa', 'tabunlupamaryhope@gmail.com', '$2y$10$jzWs1umaVgKFIWb3Z7jgzuzNxa1KmNvTYwttVrNgRxaswXnL/bhyC', '0000-00-00'),
+(2, 'test', 'test', 'test@gmail.com', '$2y$10$JhbpnGngtmKczLnBGbrfx.GcRLRQTb3t3ZWDDMg2dBbjGBI4Lz2fm', '0000-00-00'),
+(3, 'test', 'test', 'hope@gmail.com', '$2y$10$EKzdjG9Xrmel7lKfXE9XTecJfww5m7e66zcFZbKgBOuiM/zUoe/9a', '0000-00-00'),
+(4, 'qq', 'qw', 'q@gmail.com', '$2y$10$X3cheDTBsBUaTVJeQQq5iuJNmy9r/JfYFbqouFVERT0Qx.NShvMLG', '0000-00-00');
 
 --
 -- Indexes for dumped tables
@@ -105,13 +122,13 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT for table `usar`
 --
 ALTER TABLE `usar`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=20;
 
 --
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
